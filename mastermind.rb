3b2538bfab0204e6e018441b75cfde016bbb0d62
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-07-28 17:05:16 +0900

    Add checks for bulls, cows, or invalid guesses.

--- mastermind.rb
+++ mastermind.rb
@@ -14,59 +14,87 @@ class Game
     puts " - Don't think it's going to be easy, I won't tell you WHICH position or colour was correct!"
     puts "\nThere are six colours to choose from: red, green, blue, yellow, orange and pink."
     puts "\nType the first letter of the four colours you want to choose in order."
+    puts "\nFor example, if you want to guess 'red, green, blue, yellow', enter 'rgby'."
     puts "\nDuplicates are OK!"
+    puts "\nType 'quit' if you want to give up."
+    puts "\nGood luck!"
 
-    play
-  end
-
-  #prompts for and evaluates the player's guess
-  def play
     @code.guess
-
-    case @code.evaluate
-    when "win"
-      puts "You win! Would you like to play again? (y/n)"
-      gets.chomp.downcase == "y" ? Game.new : return
-    when "loose"
-      puts "You lost, too bad. Would you like to play again? (y/n)"
-      gets.chomp.downcase == "y" ? Game.new : return
-    else
-      puts @code.evaluate
-
-      play
-    end
   end
 end
 
 class Code
   #creates a new secret code and initialize @attempts to zero
   def initialize
-    new_code
     @attempts = 0
+    @colours = "rgbyop".split("")
+    new_code
   end
 
   #sets a random code
   def new_code
-    @secret_code = Array("rgby".split(""))
+    @secret_code = []
+    4.times {@secret_code.push(@colours.sample)}
   end
 
-  #prompts for guess and incriments @attempts
+  #prompts for guess, checks if win or lose, calls evaluate and incriments @attempts
   def guess
-    puts "You have #{(12 - @attempts).to_s} attempts left, what's your guess?"
-    @guess = Array(gets.chomp.downcase.split(""))
-    @attempts += 1
+    puts "\nYou have #{(12 - @attempts).to_s} attempts left, what's your guess?"
+    @guesses = Array(gets.chomp.downcase.split(""))
+
+    if @guesses == @secret_code
+      puts "\nYou win! Play again? (y/n)"
+      play_again(gets.chomp.downcase)
+    elsif @guesses = "quit".split("")
+      play_again("n")
+    elsif @attempts == 12
+      puts "\nYou lose, too bad. Play again? (y/n)"
+      play_again(gets.chomp.downcase)
+    else
+      #check that guess is formated correctly
+      if @guesses.length != 4 || @guesses.any? { |guess| !@colours.include?(guess) }
+        puts "\nYou can't guess that, try again please."
+
+        guess
+      else
+        @attempts += 1
+
+        evaluate
+
+        guess
+      end
+    end
   end
 
-  #Compares guess to secret_code. If it matches, puts "win"
-  #else when @attempts == 12 puts "loose", else for each perfect match put "bull"
-  #and each colour match put "cow"
+  #For each perfect match put "bull" and each colour match put "cow"
   def evaluate
-    if @guess == @secret_code
-      "win"
-    elsif @attempts == 12
-      "loose"
-    else
-      #compare @guess and @secret_code for cows and bulls
+    @misses = @secret_code[0..-1]
+    @bulls = 0
+    @cows = 0
+
+    @guesses.each_with_index do |guess, i|
+      if @secret_code[i] == guess
+        @bulls += 1
+        @misses.slice!(@misses.index(guess))
+      end
+    end
+
+    @guesses.each do |guess|
+      if @misses.include?(guess)
+        @cows += 1
+        @misses.slice!(@misses.index(guess))
+      end
+    end
+
+    puts "\nYou guessed #{@guesses.to_s} and got #{@bulls.to_s} Bulls and #{@cows.to_s} Cows."
+  end
+
+  def play_again(answer)
+    if answer == "y"
+      Game.new
+    elsif answer != "n"
+      puts "Please enter 'y' for yes or 'n' for no. Would you like to play again? (y/n)"
+      play_again(gets.chomp.downcase)
     end
   end
 end
