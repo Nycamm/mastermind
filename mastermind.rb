2612d3b05c47ec93722a9b2949882ae40c1e10ee
Trevor Sinnott <sinnott.trevor@gmail.com>
2018-07-29 12:14:13 +0900

    add option for player to choose secret code themselves.

--- mastermind.rb
+++ mastermind.rb
@@ -1,25 +1,49 @@
 class Game
   #creates new Code object and give instructions
   def initialize
+    instructions
+
     @code = Code.new
 
-    puts "Welcome to Mastermind!"
-    puts "\nTry to crack the secret code!"
-    puts "Guess the pattern correctly and you win!"
-    puts "\nThere are four positions in the pattern. You must choose a colour for each position."
-    puts "\nIf one of the colours you guessed is in the secret code I'll let you know!"
-    puts " - If the colour AND position are correct I will give you a 'Bull'."
-    puts " - If the colour is correct, but the position is wrong I will give you a 'Cow'."
-    puts " - You don't get anything if the colour isn't in the code at all, sorry!"
-    puts " - Don't think it's going to be easy, I won't tell you WHICH position or colour was correct!"
-    puts "\nThere are six colours to choose from: red, green, blue, yellow, orange and pink."
-    puts "\nType the first letter of the four colours you want to choose in order."
-    puts "\nFor example, if you want to guess 'red, green, blue, yellow', enter 'rgby'."
-    puts "\nDuplicates are OK!"
-    puts "\nType 'quit' if you want to give up."
-    puts "\nGood luck!"
-
     @code.guess
+  end
+
+  def instructions
+    @instructions = ["\n",
+                     "Welcome to Mastermind!",
+                     "\n",
+                     "Try to crack the secret code!",
+                     "\n",
+                     "Guess the pattern correctly and you win!",
+                     "\n",
+                     "There are four positions in the pattern. You must choose a colour for each position.",
+                     "\n",
+                     "If one of the colours you guessed is in the secret code I'll let you know!",
+                     "\n",
+                     "             - If the colour AND position are correct I will give you a 'Bull'.",
+                     "             - If the colour is correct, but the position is wrong I will give you a 'Cow'.",
+                     "             - You don't get anything if the colour isn't in the code at all, sorry!",
+                     "             - Don't think it's going to be easy, I won't tell you WHICH position or colour was correct!",
+                     "\n",
+                     "There are six colours to choose from: Red, Green, Blue, Yellow, Orange and Pink.",
+                     "\n",
+                     "Type the first letter of the four colours you want to choose in order.",
+                     "\n",
+                     "For example, if you want to guess 'red, green, blue, yellow', enter 'rgby'.",
+                     "\n",
+                     "Duplicates are OK!",
+                     "\n",
+                     "Type 'quit' if you want to give up.",
+                     "\n",
+                     "Good luck!"]
+
+    @instructions.each_with_index do |message, i|
+      if i > 10 && i < 15
+        puts message
+      else
+        puts message.center(100)
+      end
+    end
   end
 end
 
@@ -28,13 +52,29 @@ class Code
   def initialize
     @attempts = 0
     @colours = "rgbyop".split("")
-    new_code
+
+    puts "\nPlease choose your Codemaster.\nIf you would like play against the computer enter 'c'.\nTo choose a secret code yourself and challenge a friend choose 'p'."
+
+    new_code(gets.chomp.downcase)
   end
 
   #sets a random code
-  def new_code
-    @secret_code = []
-    4.times {@secret_code.push(@colours.sample)}
+  def new_code(code_master)
+    if code_master == "c"
+      @secret_code = []
+      4.times {@secret_code.push(@colours.sample)}
+    elsif code_master == "p"
+      puts "What's your four colour secret code?"
+      @secret_code = gets.chomp.downcase.split("")
+
+      if @secret_code.length != 4 || @secret_code.any? { |code| !@colours.include?(code) }
+        puts "You can't choose that."
+        new_code("p")
+      end
+    else
+      puts "Please enter 'c' or 'p' to choose Computer or Player."
+      new_code(gets.chomp.downcase)
+    end
   end
 
   #prompts for guess, checks if win or lose, calls evaluate and incriments @attempts
@@ -45,10 +85,10 @@ class Code
     if @guesses == @secret_code
       puts "\nYou win! Play again? (y/n)"
       play_again(gets.chomp.downcase)
-    elsif @guesses = "quit".split("")
+    elsif @guesses == "quit".split("")
       play_again("n")
-    elsif @attempts == 12
-      puts "\nYou lose, too bad. Play again? (y/n)"
+    elsif @attempts == 11
+      puts "\nYou lose, too bad. The secret code was #{@secret_code.to_s} Play again? (y/n)"
       play_again(gets.chomp.downcase)
     else
       #check that guess is formated correctly
@@ -74,15 +114,21 @@ class Code
 
     @guesses.each_with_index do |guess, i|
       if @secret_code[i] == guess
-        @bulls += 1
-        @misses.slice!(@misses.index(guess))
+        @misses[i] = "bull"
       end
     end
 
     @guesses.each do |guess|
       if @misses.include?(guess)
+        @misses[@misses.index(guess)] = "cow"
+      end
+    end
+
+    @misses.each do |hit|
+      if hit == "bull"
+        @bulls += 1
+      elsif hit == "cow"
         @cows += 1
-        @misses.slice!(@misses.index(guess))
       end
     end
 
